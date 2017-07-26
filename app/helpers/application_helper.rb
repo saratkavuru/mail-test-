97951188f5ec6965e56129209296fddf2ece4401
saratkavuru <kavurusarat@gmail.com>
2017-07-25 22:59:53 -0500

    Something better than nothing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,18 @@
 module ApplicationHelper
+  def get_anonymous_mode
+    session[:mode]
+  end
   def set_anonymous_mode
-    $mode= !$mode
+    session[:mode]= !session[:mode]
+  end
+  def anonymize(name)
+    (name.sum)%50
+  end
+  def display_anon_name(name)
+    if (session[:mode])
+      return anonymize(name)
+    else
+      return name
+    end
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,3 @@
 module UsersHelper
-def anonymize(name)
-  (name[0].ord + name[-1].ord + name.length)%5
-end
+
 end
