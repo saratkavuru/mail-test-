20043bef758da8c357c9893ab0e5d825220f6759
saratkavuru <kavurusarat@gmail.com>
2017-07-26 19:31:37 -0500

    There !!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,25 @@
 module ApplicationHelper
-  def get_anonymous_mode
+  def is_anonymous_mode?
     session[:mode]
   end
   def anonymize(name)
     (name.sum)%5
   end
-  def anonymize_fullname(name)
-    if (session[:mode])
+  def display_anon_fullname(name)
     temp=name.split(',')
-    return AnonymizedFirstName.find(anonymize(temp[0])).name+", "+AnonymizedLastName.find(anonymize(temp[-1])).name
-    else
-      return name
-      end
+    return AnonymizedFirstName.find(anonymize(temp[0])).name+", "+AnonymizedLastName.find(anonymize(temp[-1])).name if is_anonymous_mode?
+    return name
   end
   def display_anon_name(name)
-    if (session[:mode])
-      return AnonymizedFirstName.find(anonymize(name)).name
-    else
+      return AnonymizedFirstName.find(anonymize(name)).name if is_anonymous_mode?
       return name
-    end
+  end
+  def display_anon_mail(name)
+      return display_anon_handle(name) + "@address.com" if is_anonymous_mode?
+      return name
+  end
+  def display_anon_handle(name)
+    return AnonymizedFirstName.find(anonymize(name)).name.downcase if is_anonymous_mode?
+    return name
   end
 end
